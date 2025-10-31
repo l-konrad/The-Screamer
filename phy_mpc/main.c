938dee83faec75709f5f6bc8062d76aa0552b5cb
lkonrad <lkonrad@student.tgm.ac.at>
2025-10-31 00:46:47 +0000

    news

--- phy_mpc/main.c
+++ phy_mpc/main.c
@@ -1,24 +1,7 @@
 
 /****************************************************************************
- * apps/examples/buttons/buttons_main.c
- *
- * SPDX-License-Identifier: Apache-2.0
- *
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.  The
- * ASF licenses this file to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance with the
- * License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
- * License for the specific language governing permissions and limitations
- * under the License.
- *
+ * phy_mpc/main.c
+ * LUVLEY
  ****************************************************************************/
 
 /****************************************************************************
@@ -334,11 +317,18 @@ static int button_daemon(int argc, char *argv[])
           if ((sample & (1 << i)) && !(oldsample & (1 << i)))
             {
               printf("%s was pressed\n", button_name[i]);
+
+
             }
 
           if (!(sample & (1 << i)) && (oldsample & (1 << i)))
             {
               printf("%s was released\n", button_name[i]);
+              if ((strcmp( button_name[i], "Play/Pause")==0) ){
+                printf("Stopping/Playing Music");
+				system("sh /data/test.sh");
+
+              }
             }
         }
 
